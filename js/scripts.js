9b2edda082b5d0549dae305bc89ecdc16056e12b
taylorlin92 <omgitspuzzlekid@gmail.com>
2024-06-19 10:07:54 +0800

    Checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,30 @@
-const burger = document.querySelector('.burger'); // 選取漢堡圖示元素
-const nav = document.querySelector('.nav-links'); // 選取導航連結容器
-const navLinks = document.querySelectorAll('.nav-links li'); // 選取所有導航連結列表項目
-const header = document.querySelector('header'); // 選取頁首元素
-const logo = document.querySelector('.logo img'); // 選取 logo 圖片元素
-
-// 點擊漢堡圖示時的事件處理
-burger.addEventListener('click', () => {
-    nav.classList.toggle('active'); // 切換導航連結容器的顯示狀態
-
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = ''; // 如果有動畫，則清除動畫
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`; // 設置動畫效果
-        }
+(function ($) {
+    "use strict";
+  
+    // NAVBAR
+    $('.navbar-collapse a').on('click', function () {
+      $(".navbar-collapse").collapse('hide');
     });
 
-    burger.classList.toggle('toggle'); // 切換漢堡圖示的樣式，讓其變成叉叉
-});
+    $(document).ready(function () {
+        var lastScrollTop = 0;
+        $(window).scroll(function () {
+          var st = $(this).scrollTop();
+          if (st > lastScrollTop) {
+            // 向下滚动
+            $('.navbar').addClass('hidden');
+          } else {
+            // 向上滚动
+            $('.navbar').removeClass('hidden').addClass('navbar-logo-only');
+          }
+          lastScrollTop = st;
+        });
+  
+        // 初始化时显示 LOGO 和汉堡菜单
+        $('.navbar').addClass('navbar-logo-only');
+      });
 
-// 滾動頁面時的事件處理
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled'); // 如果滾動距離超過50px，添加scrolled類別
-        logo.style.opacity = 1; // 顯示 logo
-        logo.style.visibility = 'visible'; // 設置 logo 為可見
-    } else {
-        header.classList.remove('scrolled'); // 如果滾動距離小於50px，移除scrolled類別
-        logo.style.opacity = 0; // 隱藏 logo
-        logo.style.visibility = 'hidden'; // 設置 logo 為隱藏
-    }
-});
+
 
 //home page next section
 document.querySelectorAll('.filter-menu button').forEach(button => {
@@ -51,5 +45,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const year = new Date().getFullYear();
     document.getElementById('copyright').innerHTML = `&copy; ${year} Taylor Co., Ltd. All rights reserved.`;
 });
-
+})(window.jQuery);
 
