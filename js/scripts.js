8d3a4970289cf8989f0fd836ac626817a164d409
taylorlin92 <omgitspuzzlekid@gmail.com>
2024-06-24 17:40:29 +0800

    about.html draft

--- js/scripts.js
+++ js/scripts.js
@@ -58,6 +58,25 @@ $(document).ready(function() {
     });
 });
 
+// CUSTOM LINK
+$('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 60;
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+  });
 
 })(window.jQuery);
 
