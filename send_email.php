2d732f5c356753f9338eb844b922ad840305e173
taylorlin92 <omgitspuzzlekid@gmail.com>
2024-11-11 04:07:41 +0800

    E-mail change

--- send_email.php
+++ send_email.php
@@ -1,35 +1,47 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Collect form data
-    $name = htmlspecialchars($_POST['name']);
-    $surname = htmlspecialchars($_POST['surname']);
-    $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
+    $name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+    $surname = isset($_POST['surname']) ? htmlspecialchars($_POST['surname']) : '';
+    $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+    $subject = isset($_POST['subject']) ? htmlspecialchars($_POST['subject']) : '';
+    $message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
 
-    // Your email address where you want to receive the data
-    $to = "TAYLOR@YOKOTEX.COM";
-    
-    // Subject for the email
-    $email_subject = "New Inquiry from $name $surname: $subject";
+    // Recipient email address
+    $to = "TAYLOR@YOKOTEX.COM";  // Replace with your actual email address
 
-    // Email body
-    $email_body = "Name: $name $surname\n";
-    $email_body .= "Email: $email\n";
-    $email_body .= "Subject: $subject\n";
-    $email_body .= "Message:\n$message\n";
+    // Email subject
+    $email_subject = "New Message from: $name $surname";
 
-    // Headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+    // Construct the email content
+    $email_body = "
+    <html>
+    <head>
+        <title>$email_subject</title>
+    </head>
+    <body>
+        <p><strong>Name:</strong> $name $surname</p>
+        <p><strong>Email:</strong> $email</p>
+        <p><strong>Subject:</strong> $subject</p>
+        <p><strong>Message:</strong></p>
+        <p>$message</p>
+    </body>
+    </html>
+    ";
 
-    // Send email
+    // Set content-type header for HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+
+    // Additional headers
+    $headers .= "From: $email" . "\r\n";
+    $headers .= "Reply-To: $email" . "\r\n";
+
+    // Send the email
     if (mail($to, $email_subject, $email_body, $headers)) {
         echo "Message sent successfully!";
     } else {
-        echo "Failed to send message.";
+        echo "There was an error sending the message. Please try again.";
     }
-} else {
-    echo "Invalid request method.";
 }
 ?>
